15ac06812ebecbb8c0a0a9413302c206c2a472aa
hunterlan <konstantin.sharykin@outlook.com>
2025-08-25 18:42:19 +0200

    feat: implement jwt service

--- src/EventHarbour.UserService.Presentation/Services/JWTs/IJwtService.cs
+++ src/EventHarbour.UserService.Presentation/Services/JWTs/IJwtService.cs
@@ -1,6 +1,8 @@
+using EventHarbout.UserService.Models;
+
 namespace EventHarbour.UserService.Presentation.Services.JWTs;
 
 public interface IJwtService
 {
-    
+    string GenerateToken(User user);
 }
--- src/EventHarbour.UserService.Presentation/Services/JWTs/JwtService.cs
+++ src/EventHarbour.UserService.Presentation/Services/JWTs/JwtService.cs
@@ -1,14 +1,22 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using EventHarbour.UserService.Presentation.Options;
 using EventHarbout.UserService.Models;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace EventHarbour.UserService.Presentation.Services.JWTs;
 
 public class JwtService : IJwtService
 {
-    public string GenerateToken()
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtService(IOptions<JwtOptions> optionsJwt)
+    {
+        _jwtOptions = optionsJwt.Value;
+    }
+    public string GenerateToken(User user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var rsa = LoadRsaKey();
@@ -20,23 +28,33 @@ public class JwtService : IJwtService
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-
+            Subject = CreateClaims(user),
+            Issuer = _jwtOptions.Issuer,
+            Audience = _jwtOptions.Audience,
+            SigningCredentials = signingCredentials,
+            Expires = DateTime.UtcNow.AddMinutes(_jwtOptions.ExpireAt)
         };
         
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
     }
 
-    private List<Claim> CreateClaims(User user)
+    private ClaimsIdentity CreateClaims(User user)
     {
-        throw new NotImplementedException();
+        var claims = new ClaimsIdentity();
+        claims.AddClaim(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        claims.AddClaim(new Claim(JwtRegisteredClaimNames.Sub, user.Username));
+        claims.AddClaim(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.AddClaim(new Claim(ClaimTypes.Role, user.Role.Title));
+        
+        return claims;
     }
 
     private RSA LoadRsaKey()
     {
         var rsa = RSA.Create();
         
-        var pemContents = File.ReadAllText("");
+        var pemContents = File.ReadAllText(_jwtOptions.RsaKey.PrivatePath);
         rsa.ImportFromPem(pemContents);
         
         return rsa;
