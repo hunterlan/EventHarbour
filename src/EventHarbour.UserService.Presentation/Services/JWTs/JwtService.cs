144c4cc89403066396b82e88ef79e16c82007b4e
hunterlan <konstantin.sharykin@outlook.com>
2025-08-25 16:51:14 +0200

    Prepare for implementation

--- src/EventHarbour.UserService.Presentation/Services/JWTs/JwtService.cs
+++ src/EventHarbour.UserService.Presentation/Services/JWTs/JwtService.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using EventHarbout.UserService.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EventHarbour.UserService.Presentation.Services.JWTs;
 
@@ -6,11 +10,35 @@ public class JwtService : IJwtService
 {
     public string GenerateToken()
     {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var rsa = LoadRsaKey();
+
+        var signingCredentials = new SigningCredentials(
+            new RsaSecurityKey(rsa),
+            SecurityAlgorithms.RsaSha256
+            );
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+
+        };
+        
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    private List<Claim> CreateClaims(User user)
+    {
         throw new NotImplementedException();
     }
 
     private RSA LoadRsaKey()
     {
-        throw new NotImplementedException();
+        var rsa = RSA.Create();
+        
+        var pemContents = File.ReadAllText("");
+        rsa.ImportFromPem(pemContents);
+        
+        return rsa;
     }
 }
